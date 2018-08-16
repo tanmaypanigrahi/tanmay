40aa1c818c6d4ef8bdd2875cc556c15a40d96af1
tanmaypanigrahi <42138533+tanmaypanigrahi@users.noreply.github.com>
2018-08-16 20:36:13 +0530

    Add files via upload

---
+++ tanu1.cpp
@@ -0,0 +1,26 @@
+//library
+#include <iostream>
+using namespace std;
+//writing main function
+int main()
+{
+//declaring variables
+int a=4,b=2;
+int c,d,e,f,g;
+//adding two numbers
+c=a+b;
+//substracting two numbers
+d=a-b;
+//multiplying two numbers
+e=a*b;
+//dividing two numbers
+f=a/b;
+//remainder of two numbers
+g=a%b;
+cout<<"sum of two numbers ="<< c;
+cout<<"difference of two numbers="<< d << endl;
+cout<<"product of two number="<< e << endl;
+cout<<"division of two numbers="<< f << endl;
+cout<<"remainder of two numbers="<< g << endl;
+return 0;
+}
