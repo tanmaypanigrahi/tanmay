64e06d3828d1fef5dffa9449b7861a89b55ec9e1
tanmaypanigrahi <42138533+tanmaypanigrahi@users.noreply.github.com>
2018-08-16 20:23:30 +0530

    Add files via upload

---
+++ tanu.cpp
@@ -0,0 +1,18 @@
+//library
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;//declaring two integer variables at a time
+double d;//declarring varible of double datatype
+float f;//declaring float variable
+bool boolb;//declaring boolean variable
+char charc ;//declaring character value
+//displayig size of different datatypes
+cout<<"size of integer datatype is"<<sizeof(a)<<endl;
+cout<<"size of double  datatype is"<<sizeof(d)<<endl;
+cout<<"size of float  datatype is"<<sizeof(f)<<endl;
+cout<<"size of boolean datatype is"<<sizeof(boolb)<<endl;
+cout<<"size of character datatype is"<<sizeof(charc)<<endl;
+return 0;
+}
