58d391ded1e9a646a37a680f89c7604451fd232f
tanmaypanigrahi <42138533+tanmaypanigrahi@users.noreply.github.com>
2018-08-22 00:53:31 -0400

    Add files via upload

--- tanu1.cpp
+++ tanu1.cpp
@@ -1,26 +1,14 @@
-//library
-#include <iostream>
+#include<iostream>
 using namespace std;
-//writing main function
 int main()
 {
-//declaring variables
-int a=4,b=2;
-int c,d,e,f,g;
-//adding two numbers
-c=a+b;
-//substracting two numbers
-d=a-b;
-//multiplying two numbers
-e=a*b;
-//dividing two numbers
-f=a/b;
-//remainder of two numbers
-g=a%b;
-cout<<"sum of two numbers ="<< c;
-cout<<"difference of two numbers="<< d << endl;
-cout<<"product of two number="<< e << endl;
-cout<<"division of two numbers="<< f << endl;
-cout<<"remainder of two numbers="<< g << endl;
+double c,f;
+//to enter temperature in celcius
+cout<<"enter temperature in celcius";
+cin>> c;
+//to convert into fahrenheit
+f=(1.8*c)+32;
+//to display temperature in fahrenheit
+cout<<"temperature in fahreiheit="<<f<<endl;
 return 0;
 }
